6a510cf8f285a5d826d6f31a1064476de206777e
sartre-mauro <sartre_omar@hotmail.com>
2016-04-19 11:19:08 +0200

    Versioning System: base config

---
+++ recipes01/hello-ml.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+
+# smooth  -> 1
+# bumpy   -> 0
+features = [ [140, 1], [130, 1], [150, 0], [170, 0] ]
+# apple  -> 0
+# orange -> 1
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print(clf.predict([ [160, 0] ]))
